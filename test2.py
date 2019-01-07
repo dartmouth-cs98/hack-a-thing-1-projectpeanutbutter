6b430754773388f357ded085e9dc5f001fad5918
John Kotz <john.lyme@mac.com>
2019-01-07 12:04:29 -0500

    Working on a bunch of awesome voice and nlp stuff

--- test2.py
+++ test2.py
@@ -1,1 +1,14 @@
-print("hi")
+import speech_recognition as sr
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+with mic as source:
+    r.adjust_for_ambient_noise(source, duration=0.5)
+    print("Say something!")
+    audio = r.listen(source)
+    
+
+text = r.recognize_google(audio)
+
+print(text)
